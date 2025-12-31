18a28abb9b6bf843972b3c3673371a808e850fde
Alisher Iskakov <pakapaka1995@mail.ru>
2025-12-31 06:24:53 +0100

    Added priority to logo

--- components/header.tsx
+++ components/header.tsx
@@ -11,6 +11,7 @@ export default function Header() {
           alt="Mobile phone with posts feed on it"
           width={100}
           height={100}
+          priority
         />
       </Link>
       <nav>
