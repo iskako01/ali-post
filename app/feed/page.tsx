be089a9269b0cb84ecb457aaae02027fb5e21a68
Alisher Iskakov <pakapaka1995@mail.ru>
2026-01-04 12:24:56 +0100

    Added metadata

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,15 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/post";
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+
+  return {
+    title: `Browse all our ${posts.length} posts`,
+    description: "Browse all our posts",
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
 
