808c1e76f999e23929d24bb6b3930624dd7cd52a
Alisher Iskakov <pakapaka1995@mail.ru>
2026-01-06 05:43:13 +0100

    Configuring  Session and Session Cookie

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { Lucia } from "lucia";
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import db from "./traningdb";
+import { cookies } from "next/headers";
 
 const adapter = new BetterSqlite3Adapter(db, {
   user: "users",
@@ -15,3 +16,14 @@ const lucia = new Lucia(adapter, {
     },
   },
 });
+
+export async function createAuthSession(userId: string) {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  (await cookies()).set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
